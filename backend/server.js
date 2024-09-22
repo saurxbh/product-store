98ecf2af43f58954888b63ac4d40b0cc66b7e3cc
saurxbh <36542272+saurxbh@users.noreply.github.com>
2024-09-21 21:06:20 -0400

    Create Product Model

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from "dotenv"
+import { connectDB } from './config/db.js';
 
 dotenv.config();
 
@@ -9,9 +10,9 @@ app.get("/products", (req, res) => {
     res.send("Server is ready.")
 })
 
-console.log(process.env.MONGO_URI);
 
 app.listen(8080, () => {
+    connectDB();
     console.log("server started at http://localhost:8080");
 })
 
