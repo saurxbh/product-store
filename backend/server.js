3ecac23175057c0e32917b39d688a29206a48a5b
saurxbh <36542272+saurxbh@users.noreply.github.com>
2024-09-21 20:49:22 -0400

    Set up database connection

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 import express from 'express';
+import dotenv from "dotenv"
+
+dotenv.config();
 
 const app = express();
 
 app.get("/products", (req, res) => {
     res.send("Server is ready.")
 })
+
+console.log(process.env.MONGO_URI);
 
 app.listen(8080, () => {
     console.log("server started at http://localhost:8080");
