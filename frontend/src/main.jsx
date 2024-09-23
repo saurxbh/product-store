728a5d8cdf221a4917779a92367aee79689bd01f
saurxbh <36542272+saurxbh@users.noreply.github.com>
2024-09-23 16:45:18 -0400

    Fix Import

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter } from "react-router-dom"; 
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <ChakraProvider>
     <BrowserRouter>
       <App />
