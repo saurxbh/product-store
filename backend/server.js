1bb1e2542d8a62fe7154bc120dae1055863f21f5
saurxbh <36542272+saurxbh@users.noreply.github.com>
2024-09-22 21:27:43 -0400

    Create a Product

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,37 @@
 import express from 'express';
 import dotenv from "dotenv"
 import { connectDB } from './config/db.js';
+import Product from './models/product.model.js';
 
 dotenv.config();
 
 const app = express();
 
-app.get("/products", (req, res) => {
-    res.send("Server is ready.")
+app.use(express.json()); // Allows us to accept JSON data in req.body
+
+app.post("/api/products", async (req, res) => {
+    const product = req.body; // User will send this data
+    if(!product.name || !product.price || !product.image) {
+        return res.status(400).json({
+            success:false,
+            message:"Please provide all the fields"
+        });
+    }
+    const newProduct = new Product(product);
+
+    try {
+        await newProduct.save();
+        return res.status(201).json({
+            success: true,
+            data: newProduct
+        });
+    } catch(error) {
+        console.error("Error while creating product", error.message);
+        return res.status(500).json({
+            success: false,
+            message: "Server Error"
+        });
+    }
 })
 
 
