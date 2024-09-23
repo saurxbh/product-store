608e95b088a8af4aba19eed1a4356ed90f291821
saurxbh <36542272+saurxbh@users.noreply.github.com>
2024-09-23 16:36:02 -0400

    Create Home Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
-import { Button } from "@chakra-ui/react"
+import { Box } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
+import CreatePage from './pages/CreatePage.jsx';
+import HomePage from './pages/HomePage.jsx';
+import Navbar from './components/Navbar.jsx';
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    </>
+    <Box minH={"100vh"}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+      </Routes>
+    </Box>
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import { ChakraProvider } from '@chakra-ui/react'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter } from "react-router-dom"; 
 
 createRoot(document.getElementById('root')).render(
   <ChakraProvider>
