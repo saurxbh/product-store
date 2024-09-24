44e23b1c6669472513cc9a51ed981a18f81204a2
saurxbh <36542272+saurxbh@users.noreply.github.com>
2024-09-24 11:00:11 -0400

    Delete Product From Product Card and Update UI

--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -2,7 +2,9 @@ import { create } from "zustand";
 
 export const useProductStore = create((set) => ({
     products: [],
+
     setProducts: (products) => set({products}),
+
     createProduct: async (newProduct) => {
         if (!newProduct.name || !newProduct.image || !newProduct.price) {
             return {success:false, message:"Please fill in all the fields."}
@@ -18,9 +20,22 @@ export const useProductStore = create((set) => ({
         set((state) => ({products:[...state.products, data.data]}));
         return {success: true, message: "Product Created Successfully"};
     },
+
     fetchProducts: async () => {
         const res = await fetch("/api/products");
         const data = await res.json();
         set({ products: data.data});
+    },
+
+    deleteProduct: async (pid) => {
+        const res = await fetch(`/api/products/${pid}`, {
+            method: "DELETE",
+        });
+        const data = await res.json();
+        if (!data.success) return { success: false, message: data.message };
+
+        // update the ui immediately without needing a refresh
+        set((state) => ({ products: state.products.filter((product) => product._id !== pid) }));
+        return { success: true, message: data.message };
     }
 }));
