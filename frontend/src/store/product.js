9c83b56c9b3ddb8087750167c307e708e2d618e1
saurxbh <36542272+saurxbh@users.noreply.github.com>
2024-09-24 00:45:38 -0400

    Create Product Cards

--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -17,5 +17,10 @@ export const useProductStore = create((set) => ({
         const data = await res.json();
         set((state) => ({products:[...state.products, data.data]}));
         return {success: true, message: "Product Created Successfully"};
+    },
+    fetchProducts: async () => {
+        const res = await fetch("/api/products");
+        const data = await res.json();
+        set({ products: data.data});
     }
 }));
