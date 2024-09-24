78fc64e66a72c7e56b9ab34c6c208d96cbed6681
saurxbh <36542272+saurxbh@users.noreply.github.com>
2024-09-23 21:14:32 -0400

    Create A Product

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,6 +7,7 @@ import { LuSun } from "react-icons/lu";
 
 const Navbar = () => {
   const { colorMode, toggleColorMode} = useColorMode();
+
   return (
     <Container maxW={"1140px"} px={4}>
       <Flex 
--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,5 +1,6 @@
 import { Box, Button, Container, Heading, Input, useColorModeValue, VStack } from '@chakra-ui/react';
 import React, { useState } from 'react'
+import { useProductStore } from '../store/product';
 
 const CreatePage = () => {
   const [newProduct, setNewProduct] = useState({
@@ -8,8 +9,11 @@ const CreatePage = () => {
     image: ""
   });
 
-  const handleAddProduct = () => {
-    console.log(newProduct);
+  const {createProduct} = useProductStore()
+  const handleAddProduct = async () => {
+    const { success, message } = await createProduct(newProduct);
+    console.log("Success:", success);
+    console.log("Message:", message);
   };
 
   return (
